64bf64e23b59a795a8272471b65479622250f8ce
grcbrown <122645861+grcbrown@users.noreply.github.com>
2024-09-30 11:22:31 -0700

    adjusted intensity of audio check, first pass custom trial order in timeline

--- experiments/list_1/experiment.js
+++ experiments/list_1/experiment.js
@@ -128,7 +128,31 @@ const audio_trials = {
         }
     ],
     timeline_variables: trial_obj,
-    randomize_order: true
+    sample: {
+        type: 'custom',
+        fn: function(){
+            //define groups
+            var groups = [[0,1,2,3,4,5],[6,7,8,9,10,11],[12,13,14,15,16,17],[18,19,20,21,22,23],[24,25,26,27,28,29],[30,31,32,33,34,35],[36,37,38,39,40,41],[42,43,44,45,46,47],[48,49,50,51,52,53],[54,55,56,57,58,59],[60,61,62,63,64,65],[66,67,68,69,70,71],[72,73,74,75,76,77],[78,79,80,81,82,83],[84,85,86,87,88,89],[90,91,92,93,94,95],[96,97,98,99,100,101],[102,103,104,105,106,107]];
+
+            //create object for each timeline variable specifying group membership and item
+            var all_items = [];
+            for(var i=0; i<groups.length; i++){
+                for(var j=0; j<groups[i].length; j++){
+                    all_items.push({group: i, trial_num:groups[i][j]});
+                }
+            }
+
+            //randomize group order, with no repeat constraint
+            //use custom equality function to specify what a "repeat" means
+            var order = jsPsych.randomization.shuffleNoRepeats(all_items, function(a,b){
+                return a.group == b.group;
+            });
+            
+            //create the final list of trials
+            return order.map(function(x){return x.trial_num});
+        }
+    }
+    //randomize_order: true
 };
 timeline.push(audio_trials);
 
