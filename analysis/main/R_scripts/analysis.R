1bd01c54e03dc15dc5168f9d70af1f8d8b71e5ea
grcbrown <122645861+grcbrown@users.noreply.github.com>
2024-10-06 20:50:24 -0700

    experimental data uploaded, data analysis up to regression models

--- analysis/main/R_scripts/analysis.R
+++ analysis/main/R_scripts/analysis.R
@@ -11,8 +11,8 @@ cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00",
 theme_set(theme_bw())
 
 # LOAD DATA
-raw_data <- read.csv('./data/qp1_experiment_1-merged.csv')
-survey <- read.csv('./data/survey.csv')
+raw_data <- read.csv('./data/qp1_e1-merged.csv')
+survey <- read.csv('./data/survey_post.csv')
 data <- left_join(x=raw_data,y=survey,by="workerid")
 
 # DATA SHAPING
@@ -20,13 +20,13 @@ data <- left_join(x=raw_data,y=survey,by="workerid")
 ## (1) Audio attention check
 exclude_audio <- data %>% filter(trial_type == "audio-button-response") %>% group_by(workerid) %>% filter(response != 2) 
 print(exclude_audio$workerid)
-data <- data[!(data$workerid %in% c(568, 570)),]
+data <- data[!(data$workerid %in% c(728, 707, 697, 601, 832, 688, 631, 817, 853, 733, 646, 841, 828, 726, 734, 849, 772, 822, 594)),]
 ## (2) - responded to less than 85% of trials 
 trial_num <- 108 #108 total trials (54 sib + 54 control)
 exclude_trial <- data %>% filter(!is.na(rt)) %>% filter(trial_type == "audio-slider-response") %>% group_by(workerid) %>% count(workerid)
-exclude2 <- exclude_trial %>% filter(n/trial_num > 0.85)
+exclude2 <- exclude_trial %>% filter(n/trial_num < 0.85)
 print(exclude2$workerid)
-data <- data[!(data$workerid %in% c(571, 573)),]n_distinct(data$workerid)
+data <- data[!(data$workerid %in% c(866)),]
 total_respondants <- n_distinct(data$workerid)
 
 ## separate numeric and string data
@@ -64,13 +64,21 @@ score_gender_trans <- data %>% filter(!is.na(gender_trans)) %>% group_by(workeri
 data <- merge(data, score_gender_trans, by = "workerid", all.x = TRUE)
 srq_score <- data %>% group_by(workerid) %>% summarize("score" = mean(score_trans+score_link))
 
+## relable spk
+data$spk[data$spk == 246] = "non-masc"
+data$spk[data$spk == 723] = "masc"
+data$spk[data$spk == 340] = "neut"
+
 ### summarize numeric data 
 exp_data <- data %>% 
   filter(!is.na(response)) %>% 
   group_by(spk) %>% 
-  filter(spk == 246 | spk == 340 | spk == 723)
+  filter(spk == "non-masc" | spk == "neut" | spk == "masc")
 
 exp_data <- filter(exp_data, is.na(response_numeric)==FALSE)
+
+exp_data$sib_code <- factor(exp_data$sib_code, levels = c("control", "low", "mid", "high"))
+
 #### by expected + triplet_id
 exp_sub_1 <- subset(exp_data, select = -c(trial_index,workerid)) 
 exp_summary_1 <- summarize(exp_sub_1, "mean"=mean(response_numeric/100), "var" = var(response_numeric/100), "sd" = sd(response_numeric/100))
@@ -78,12 +86,12 @@ print(exp_summary_1)
 
 #### by expected + participant 
 exp_sub_2 <- subset(exp_data, select = -c(trial_index,triplet_id)) 
-exp_summary_2 <- summarize(exp_sub_2, "mean"=mean(response_numeric), "var" = var(response_numeric))
+exp_summary_2 <- summarize(exp_sub_2, "mean"=mean(response_numeric/100), "var" = var(response_numeric/100))
 print(exp_summary_2)
 
 # VISUALIZATIONS 
 ## gen distribution of ratings 
-hist_all <- ggplot(exp_data, aes(x=response_numeric)) + 
+hist_all <- ggplot(exp_data, aes(x=response_numeric/100)) + 
   geom_histogram(bins=30) +
   xlab("Masculinity Rating") + 
   scale_fill_manual(values = cbPalette) + 
@@ -101,7 +109,7 @@ print(hist_by_spk)
 ggsave(file="./analysis/main/Graphs/hist_spk.pdf",width=7,height=4)
 ggsave(file="./analysis/main/Graphs/hist_spk.png",width=,height=4)
 ## overall distribution of ratings by sib_code
-hist_by_sib <- ggplot(exp_data,aes(x=response_numeric))+
+hist_by_sib <- ggplot(exp_data,aes(x=response_numeric/100))+
   geom_histogram(bins=30)+
   facet_grid(.~sib_code) +
   xlab("Masculinity Rating")
@@ -109,7 +117,7 @@ print(hist_by_sib)
 ggsave(file="./analysis/main/Graphs/hist_sib.pdf",width=7,height=4)
 ggsave(file="./analysis/main/Graphs/hist_sib.png",width=,height=4)
 ## speaker by sib_code
-hist_by_spk_sib <- ggplot(exp_data,aes(x=response_numeric))+
+hist_by_spk_sib <- ggplot(exp_data,aes(x=response_numeric/100))+
   geom_histogram(bins=30)+
   facet_grid(sib_code~spk) +
   xlab("Masculinity Rating")
@@ -117,67 +125,58 @@ print(hist_by_spk_sib)
 ggsave(file="./analysis/main/Graphs/hist_spk_sib.pdf",width=7,height=4)
 ggsave(file="./analysis/main/Graphs/hist_spk_sib.png",width=,height=4)
 ## faceted by-triplet average ratings 
-hist_by_trip <- ggplot(exp_data,aes(x=response_numeric))+
+hist_by_trip <- ggplot(exp_data,aes(x=response_numeric/100))+
   geom_histogram(bins=30)+
   facet_grid(.~triplet_id)
 print(hist_by_trip) 
 
 ##barplots
-
-box_all <- ggplot(exp_data,aes(y = response_numeric))+
-  geom_boxplot(aes(fill=expected))+ 
-  ylab("Masculinity Rating") +
-  scale_fill_manual("Expected Masculinity", values=cbPalette) 
-print(box_all)
-ggsave(file="./analysis/main_masc_lex/Graphs/box_all.pdf",width=6,height=4)
-ggsave(file="./analysis/main_masc_lex/Graphs/box_all.png",width=6,height=4)
-
-exp_data_bar <- exp_data %>%
-  group_by(expected) %>%
+spk_bar <- exp_data %>%
+  group_by(spk) %>%
   summarise( 
     n=n(),
-    mean=mean(response_numeric),
-    sd=sd(response_numeric)
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
   ) %>%
   mutate( se=sd/sqrt(n))  %>%
   mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
 
-bar_all <- ggplot(exp_data_bar, aes(y = mean)) +
-  geom_bar(aes(x = expected, fill = expected), stat="identity", alpha=0.7) +
-  geom_errorbar(aes(x = expected, 
+bar_spk <- ggplot(spk_bar, aes(y = mean)) +
+  geom_bar(aes(x = spk, fill = spk), stat="identity", alpha=0.7) +
+  geom_errorbar(aes(x = spk, 
                     ymin=mean-ic, 
                     ymax=mean+ic), width=0.4, colour="black", alpha=0.9) + 
   ylab("Masculinity Rating") +
-  xlab("Lexical Variant Type") +
+  xlab("Speaker ID") +
   scale_fill_manual(values = cbPalette)
-print(bar_all)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_all.pdf",width=4,height=4)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_all.png",width=4,height=4)
+print(bar_spk)
+ggsave(file="./analysis/main/Graphs/bar_spk.pdf",width=4,height=4)
+ggsave(file="./analysis/main/Graphs/bar_spk.png",width=4,height=4)
 ### by lexical_triplet
-exp_data_1 <- filter(exp_data, triplet_id < 9) %>%
-  group_by(triplet_id,expected) %>%
+exp_data_1 <- filter(exp_data, triplet_id < 8) %>%
+  group_by(triplet_id,spk) %>%
   summarise( 
     n=n(),
-    mean=mean(response_numeric),
-    sd=sd(response_numeric)
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
   ) %>%
   mutate( se=sd/sqrt(n))  %>%
   mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
-exp_data_2 <- filter(exp_data, triplet_id > 8 & triplet_id < 17)  %>%
-  group_by(triplet_id,expected) %>%
+exp_data_2 <- filter(exp_data, triplet_id > 7 & triplet_id < 16)  %>%
+  group_by(triplet_id,spk) %>%
   summarise( 
     n=n(),
-    mean=mean(response_numeric),
-    sd=sd(response_numeric)
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
   ) %>%
   mutate( se=sd/sqrt(n))  %>%
   mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
-exp_data_3 <- filter(exp_data, triplet_id > 16)  %>%
-  group_by(triplet_id,expected) %>%
+exp_data_3 <- filter(exp_data, triplet_id > 15)  %>%
+  group_by(triplet_id,spk) %>%
   summarise( 
     n=n(),
-    mean=mean(response_numeric),
-    sd=sd(response_numeric)
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
   ) %>%
   mutate( se=sd/sqrt(n))  %>%
   mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
@@ -186,44 +185,88 @@ exp_data_1$triplet_id <- as.factor(exp_data_1$triplet_id)
 exp_data_2$triplet_id <- as.factor(exp_data_2$triplet_id)
 exp_data_3$triplet_id <- as.factor(exp_data_3$triplet_id)
 
-bar_1 <- ggplot(exp_data_1, aes(x = triplet_id, y = mean, fill = expected)) +
+bar_1 <- ggplot(exp_data_1, aes(x = triplet_id, y = mean, fill = spk)) +
   stat_summary(fun = mean, geom = "bar", position = position_dodge(width = .9),
                size = 3) +
   geom_errorbar(aes(ymin=mean-ic, 
                     ymax=mean+ic), width=0.3, position = position_dodge(width = .9)) + 
   ylab("Masculinity Rating") +
   xlab("Sentence Frame") +
-  ylim(0, 1) + 
-  scale_fill_manual(values = cbPalette, name = "Lexical Variant Type")
+  scale_fill_manual(values = cbPalette, name = "Speaker")
 print(bar_1)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_1.pdf",width=8,height=5)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_1.png",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_1.pdf",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_1.png",width=8,height=5)
 
-bar_2 <- ggplot(exp_data_2, aes(x = triplet_id, y = mean, fill = expected)) +
+bar_2 <- ggplot(exp_data_2, aes(x = triplet_id, y = mean, fill = spk)) +
   stat_summary(fun = mean, geom = "bar", position = position_dodge(width = .9),
                size = 3) +
   geom_errorbar(aes(ymin=mean-ic, 
                     ymax=mean+ic), width=0.3, position = position_dodge(width = .9)) + 
   ylab("Masculinity Rating") +
   xlab("Sentence Frame") +
-  ylim(0, 1) + 
-  scale_fill_manual(values = cbPalette, name = "Lexical Variant Type")
+  scale_fill_manual(values = cbPalette, name = "Speaker")
 print(bar_2)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_2.pdf",width=8,height=5)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_2.png",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_2.pdf",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_2.png",width=8,height=5)
 
-bar_3 <- ggplot(exp_data_3, aes(x = triplet_id, y = mean, fill = expected)) +
+bar_3 <- ggplot(exp_data_3, aes(x = triplet_id, y = mean, fill = spk)) +
   stat_summary(fun = mean, geom = "bar", position = position_dodge(width = .9),
                size = 3) +
   geom_errorbar(aes(ymin=mean-ic, 
                     ymax=mean+ic), width=0.3, position = position_dodge(width = .9)) + 
   ylab("Masculinity Rating") +
   xlab("Sentence Frame") +
-  ylim(0, 1) + 
-  scale_fill_manual(values = cbPalette, name = "Lexical Variant Type")
+  scale_fill_manual(values = cbPalette, name = "Speaker")
 print(bar_3)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_3.pdf",width=8,height=5)
-ggsave(file="./analysis/main_masc_lex/Graphs/bar_3.png",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_3.pdf",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_3.png",width=8,height=5)
+
+## by sib
+sib_bar <- exp_data %>%
+  group_by(sib_code) %>%
+  summarise( 
+    n=n(),
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
+  ) %>%
+  mutate( se=sd/sqrt(n))  %>%
+  mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
+
+bar_sib <- ggplot(sib_bar, aes(y = mean)) +
+  geom_bar(aes(x = sib_code, fill = sib_code), stat="identity", alpha=0.7) +
+  geom_errorbar(aes(x = sib_code, 
+                    ymin=mean-ic, 
+                    ymax=mean+ic), width=0.4, colour="black", alpha=0.9) + 
+  ylab("Masculinity Rating") +
+  xlab("Sibilant Condition") +
+  scale_fill_manual(values = cbPalette)
+print(bar_sib)
+ggsave(file="./analysis/main/Graphs/bar_sib.pdf",width=4,height=4)
+ggsave(file="./analysis/main/Graphs/bar_sib.png",width=4,height=4)
+
+## speaker * sib
+exp_data_spk_sib <- exp_data %>%
+  group_by(sib_code,spk) %>%
+  summarise( 
+    n=n(),
+    mean=mean(response_numeric/100),
+    sd=sd(response_numeric/100)
+  ) %>%
+  mutate( se=sd/sqrt(n))  %>%
+  mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
+
+bar_spk_sib <- ggplot(exp_data_spk_sib, aes(x = sib_code, y = mean, fill = spk)) +
+  stat_summary(fun = mean, geom = "bar", position = position_dodge(width = .9),
+               size = 3) +
+  geom_errorbar(aes(ymin=mean-ic, 
+                    ymax=mean+ic), width=0.3, position = position_dodge(width = .9)) + 
+  ylab("Masculinity Rating") +
+  xlab("Sibilant Condition") +
+  scale_fill_manual(values = cbPalette, name = "Speaker")
+print(bar_spk_sib)
+ggsave(file="./analysis/main/Graphs/bar_spk_sib.pdf",width=8,height=5)
+ggsave(file="./analysis/main/Graphs/bar_spk_sib.png",width=8,height=5)
+
 
 ### SQR 
 gender_link <- ggplot(score_gender_link,aes(score_link))+geom_histogram(bins = 30)
@@ -234,14 +277,14 @@ print(gender_trans)
 # MODEL 
 ## linear mixed effects model
 exp_data$triplet_id <- as.factor(exp_data$triplet_id)
-exp_data$expected <- as.factor(exp_data$expected)
-exp_data$participant_id <- as.factor(exp_data$participant_id)
-exp_data$trial_index <- as.factor(exp_data$trial_index)
+exp_data$spk <- as.factor(exp_data$spk)
+exp_data$workerid <- as.factor(exp_data$workerid)
 exp_data$score_link <- as.numeric(exp_data$score_link)
 exp_data$score_trans <- as.numeric(exp_data$score_trans)
+exp_data
 
-exp_data$expected <- relevel(exp_data$expected, ref = "neutral")
-model_all <- lmer(response_numeric ~ expected + (1+expected|participant_id) + (1+expected|trial_index), data = exp_data)
+
+model_all <- lmer(response_numeric ~ sib_code*spk + (1+expected|workerid) + (1+expected|triplet_id) + (), data = exp_data)
 summary(model_all)
 
 r.squaredGLMM(model_all)
